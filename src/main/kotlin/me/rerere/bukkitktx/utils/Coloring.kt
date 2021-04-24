649874386ef68836380b256d074b236d7a7f4523
jiangdashao <qq1609403959@outlook.com>
2021-04-24 10:50:57 +0800

    Improve Coloring: support change color code

--- src/main/kotlin/me/rerere/bukkitktx/utils/Coloring.kt
+++ src/main/kotlin/me/rerere/bukkitktx/utils/Coloring.kt
@@ -5,13 +5,13 @@ import org.bukkit.ChatColor
 /**
  * Use '&' color codes to replace color variables of string
  */
-fun String.coloring() : String{
-    return ChatColor.translateAlternateColorCodes('&', this)
+fun String.coloring(colorCode : Char = '&') : String{
+    return ChatColor.translateAlternateColorCodes(colorCode, this)
 }
 
 /**
  * Use '&' color codes to replace color variables of string collections
  */
-fun Collection<String>.coloring() : Collection<String>{
-    return this.map { it.coloring() }
+fun Collection<String>.coloring(colorCode : Char = '&') : Collection<String>{
+    return this.map { it.coloring(colorCode) }
 }
